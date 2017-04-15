0c93efb89e6223f4336533355faae859deed2e3e
reeson2003 <reeson2003@gmail.com>
2017-04-15 12:28:42 +0300

    Initial commit

---
+++ src/main/java/ru/reeson2003/diystore/MainTest.java
@@ -0,0 +1,49 @@
+package ru.reeson2003.diystore;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by reeson on 15.04.17.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        String url = "jdbc:h2:./h2db";
+        String user = "admin";
+        String password = "12345678";
+        dataSource.setUrl(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            BufferedReader reader = new BufferedReader(new FileReader("query.sql"));
+            StringBuilder sb = new StringBuilder();
+            while (true){
+                String s = reader.readLine();
+                if (s != null)
+                    sb.append(s);
+                else
+                    break;
+            }
+            statement.execute(sb.toString());
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
