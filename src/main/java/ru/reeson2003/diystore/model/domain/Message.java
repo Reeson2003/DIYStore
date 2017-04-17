028aae3e1c3b396d42fb64533b336af662294e0e
Reeson2003 <reeson2003@gmail.com>
2017-04-17 23:09:38 +0300

    UML, user stories. Добавлен id в Message

--- src/main/java/ru/reeson2003/diystore/model/domain/Message.java
+++ src/main/java/ru/reeson2003/diystore/model/domain/Message.java
@@ -5,8 +5,17 @@ package ru.reeson2003.diystore.model.domain;
  * @author Pavel Gavrilov
  */
 public class Message {
+    private long id;
     private String message;
     private Product product;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getMessage() {
         return message;
