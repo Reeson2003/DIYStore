e2002028688d32cb2bedc6d2e0783a255f704cf5
Reeson2003 <reeson2003@gmail.com>
2017-04-22 11:17:20 +0300

    test package

---
+++ src/test/java/ru/reeson2003/diystore/model/service/in_memory_manager/InMemoryProductManagerTest.java
@@ -0,0 +1,11 @@
+package ru.reeson2003.diystore.model.service.in_memory_manager;
+
+/**
+ * Date: 22.04.2017.
+ * Time: 11:08.
+ *
+ * @author Pavel Gavrilov.
+ */
+public class InMemoryProductManagerTest {
+    private InMemoryProductManager manager;
+}
