a275d8de47543fe9ef2f23b07f311400d202b85a
Reeson2003 <reeson2003@gmail.com>
2017-05-01 17:15:32 +0300

    database create

---
+++ src/main/java/ru/reeson2003/diystore/model/service/DBCreate.java
@@ -0,0 +1,19 @@
+package ru.reeson2003.diystore.model.service;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Date: 30.04.2017.
+ * Time: 21:50.
+ *
+ * @author Pavel Gavrilov.
+ */
+public class DBCreate {
+    public static void main(String[] args) {
+        SessionFactory factory = null;
+        factory = new Configuration().configure().buildSessionFactory();
+        factory.close();
+    }
+}
